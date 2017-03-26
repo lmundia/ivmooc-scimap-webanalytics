14c0e3435507fb5b22f55ad77525fbaa72d64056
Avadhoot Agasti <avadhoot.agasti@gmail.com>
2017-03-25 23:45:45 -0700

    Quite a bit of changes to handle some missing tables in some HTML files.

--- src/webscraping/stats_parser.py
+++ src/webscraping/stats_parser.py
@@ -3,7 +3,9 @@ import csv
 import os
 
 data_folder = "../../data/SciMaps-Webalizer-Data/"
-out_data_folder = "../../data/output_summary/"
+out_data_folder = "../../data/output_tables/"
+
+header_written = 0
 
 def extract_data(filename):
     content = open(data_folder + filename, 'r').read()
@@ -11,63 +13,96 @@ def extract_data(filename):
 
     ATag = soup.find("a", {"name":"DAYSTATS"})
     TableElement = ATag.next_element.next_element.next_element
-    rows = []
+    rows = {}
 
     date_part = filename.replace(".html", "")
     date_part = date_part.replace("usage_", "")
     year_part = date_part[:4]
     month_part = date_part[-2:]
 
-#    for row in TableElement.find_all('tr'):
-#        rows.append([val.text.encode('utf8') for val in row.find_all('td')])
 
-    for row in TableElement.find_all('tr'):
-        curr_row = []
-        curr_row.append(year_part)
-        curr_row.append(month_part)
-        for val in row.find_all('td'):
-            curr_row.append(val.text.encode('utf8'))
-        rows.append(curr_row)
+    curr_table = 'DAYSTATS'
+    rows[curr_table] = []
+    for tag in TableElement.find_all(['tr', 'a']):
+        if(tag.name == 'a'):
+            #print tag
+            if tag.has_attr('name'):
+                if(tag['name'] == "DAYSTATS" or tag['name'] == "HOURSTATS" or tag['name']\
+                        == "TOPURLS" or tag['name'] == "TOPENTRY" or tag['name'] ==
+                    'TOPEXIT' or tag['name'] == 'TOPSITES' or tag['name'] ==
+                    'TOPREFS' or tag['name'] == 'TOPSEARCH' or tag['name']==
+                    'TOPUSERS' or tag['name'] == 'TOPAGENTS' or tag['name'] == 'TOPCTRYS'):
+                    curr_table = tag['name']
+                    rows[curr_table] = []
+        if (tag.name == 'tr'):
+            curr_row = []
+            curr_row.append(year_part)
+            curr_row.append(month_part)
+            add_row = 0
+            for val in tag.find_all('td'):
+                curr_row.append(val.text.encode('utf8'))
+                add_row = 1
+            if add_row == 1:
+                rows[curr_table].append(curr_row)
 
-    with open('output_file.csv', 'w') as f:
+    global header_written
+    for table_name in rows:
+        f = open(out_data_folder + table_name + '.csv', 'a')
         writer = csv.writer(f)
-        writer.writerows(row for row in rows if row)
-
-    out_file_names = ['daily.csv', 'hourly.csv', 'top100urls.csv',
-              'top10urlsbykbytes.csv',
-              'top10entry.csv','top10exitpages.csv','top30sites.csv',
-              'top10sites.csv','top30referres.csv','top20searchstrings.csv',
-              'top15agents.csv','top30countries.csv','extra.csv','extra.csv','extra.csv']
-
-
-    file_index = 0
-    #out_file_name_prefix = filename.replace(".html", "_")
-
-
-    f = open(out_data_folder + out_file_names[file_index], 'a')
-    writer = csv.writer(f)
-
-    index = 0
-    while(len(rows[index]) <= 2):
-        index += 1
-    while index < len(rows):
-        writer.writerow(rows[index])
-        index += 1
-        if(len(rows[index]) <= 2):
-            file_index = file_index + 1
-            if(file_index < len(out_file_names)-1):
-                f.close()
-                f = open(out_data_folder + out_file_names[file_index],'a')
-                writer = csv.writer(f)
-            while(len(rows[index]) <= 2):
-                index += 1
-                if(index == len(rows)):
-                    break
-
-    f.close()
-
-
-
+        if header_written < 11:
+            header_written += 1
+            if table_name == 'DAYSTATS':
+                writer.writerow(['Year', 'Month', 'Day', 'HitsTotal',
+                                         'HitsPct','FilesTotal','FilesPct',
+                                         'PagesTotal', 'PagesPct','VisitsTotal',
+                                         'VisitPct', 'SitesTotal','SitesPct',
+                                         'KBytesTotal', 'KBytesPct'])
+            if table_name == 'HOURSTATS':
+                writer.writerow(['Year', 'Month', 'Hous', 'HitsAvg',
+                                 'HitsTotal', 'HitsPct','FilesAvg','FilesTotal',
+                                 'FilesPct', 'PagesAvg',
+                                 'PagesTotal', 'PagesPct', 'KBytesAvg',
+                                 'KBytesTotal', 'KBytesPct'])
+            if table_name == 'TOPURLS':
+                writer.writerow(['Year', 'Month', 'Rank',
+                                 'HitsTotal', 'HitsPct',
+                                 'KBytesTotal', 'KBytesPct', 'URL'])
+            if table_name == 'TOPENTRY':
+                writer.writerow(['Year', 'Month', 'Rank',
+                                 'HitsTotal', 'HitsPct',
+                                 'VisitsTotal', 'VisitsPct', 'URL'])
+            if table_name == 'TOPEXIT':
+                writer.writerow(['Year', 'Month', 'Rank',
+                                 'HitsTotal', 'HitsPct',
+                                 'VisitsTotal', 'VisitsPct', 'URL'])
+            if table_name == 'TOPSITES':
+                writer.writerow(['Year', 'Month', 'Rank', 'HitsTotal',
+                                 'HitsPct', 'FilesTotal', 'FilesPct',
+                                 'KBytesTotal', 'KBytesPct', 'VisitsTotal',
+                                 'VisitPct', 'Hostname'])
+            if table_name == 'TOPREFS':
+                writer.writerow(['Year', 'Month', 'Rank',
+                                 'HitsTotal', 'HitsPct',
+                                 'Referrer'])
+            if table_name == 'TOPSEARCH':
+                writer.writerow(['Year', 'Month', 'Rank',
+                                 'HitsTotal', 'HitsPct',
+                                 'SearchString'])
+            if table_name == 'TOPUSERS':
+                writer.writerow(['Year', 'Month', 'Rank', 'HitsTotal',
+                                         'HitsPct','FilesTotal','FilesPct',
+                                         'KBytesTotal', 'KBytesPct','VisitsTotal',
+                                         'VisitPct', 'UserName'])
+            if table_name == 'TOPAGENTS':
+                writer.writerow(['Year', 'Month', 'Rank',
+                                 'HitsTotal', 'HitsPct',
+                                 'UserAgent'])
+            if table_name == 'TOPCTRYS':
+                writer.writerow(['Year', 'Month', 'Rank', 'HitsTotal',
+                                         'HitsPct','FilesTotal','FilesPct',
+                                         'KBytesTotal', 'KBytesPct', 'Country'])
+        writer.writerows(row for row in rows[table_name] if row)
+        f.close()
 
 for filename in os.listdir(data_folder):
     if filename.endswith(".html"):
